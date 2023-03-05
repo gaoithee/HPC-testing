fdd744ad79d982a7cbcbf7d07276b1fc4d8216dc
gaoithee <saracandussio@gmail.com>
2023-03-05 11:33:18 +0100

    TRUE policy

--- exercise2/ex2-epyc-dp-CLOSE.sh
+++ exercise2/ex2-epyc-dp-CLOSE.sh
@@ -11,12 +11,12 @@ module load mkl
 module load openBLAS/0.3.21-omp
 
 export OMP_PLACES=core
-export OMP_PROC_BIND=none
+export OMP_PROC_BIND=false
 export OMP_NUM_THREADS=64
 
 for size in {2000..20000..1000}
 do
         for i in {1..10}
-                do ./gemm_oblas_SP.x $size $size $size | grep GFLOPS >> ex2-epyc-oblas-sp-NONE.dat
+                do ./gemm_oblas.x $size $size $size | grep GFLOPS >> 4-ex2-epyc-oblas-dp-FALSE.dat
         done
 done
